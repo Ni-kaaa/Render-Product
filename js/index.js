65a35b0f811e2ba5385ac1a474afffc44fdcfaa3
Ni-kaaa <kanikaluy393@gmail.com>
2024-06-04 23:34:46 +0700

    first commit

--- js/index.js
+++ js/index.js
@@ -1,8 +1,20 @@
-import { CardComponent } from "../components/CardComponent.js";
-import { products } from "../data/product.js";
-let renderArea = document.querySelector("#renderArea");
+// import { CardComponent } from "../components/CardComponent.js";
+// // import { products } from "../data/product.js";
+// let renderArea = document.querySelector("#renderArea");
 
 // RENDER PRODUCT
-products
-  .slice(0, 10)
-  .map((product) => (renderArea.innerHTML += CardComponent(product)));
+// products
+//   .slice(0, 10)
+//   .map((product) => (renderArea.innerHTML += CardComponent(product)));
+
+import { CardComponent } from "../components/CardComponent.js";
+ let renderArea = document.querySelector("#renderArea");
+
+ const url = "http://127.0.0.1:5500/data/product.json";
+
+fetch(url)
+  .then((res) => res.json())
+  .then((datas) => {
+    let products = datas;
+    products.map((product) => (renderArea.innerHTML += CardComponent(product)));
+  });
